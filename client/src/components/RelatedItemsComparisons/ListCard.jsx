e16c689f9c5929f2822be3bae28d18a1b0392c6a
Justin <just.inchong4life@gmail.com>
2022-12-01 22:44:20 -0800

    added first styled component in List Card

--- client/src/components/RelatedItemsComparisons/ListCard.jsx
+++ client/src/components/RelatedItemsComparisons/ListCard.jsx
@@ -1,24 +1,25 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const StyledListCard = styled.div`
+  border: 2px solid black;
+  border-radius: 3px;
+  padding: 5px;
+  display: inline-block;
+  width: 250px;
+  height: 250px;
+  margin: 5% 2.5%;
+`;
 
 const ListCard = ({ name, description, price }) => {
 
-  const style = {
-    border: '2px solid black',
-    borderRadius: '3px',
-    padding: '5px',
-    display: 'inline-block',
-    maxHeight: '400px',
-    maxWidth: '400px',
-    marginLeft: '10px',
-  };
-
   return (
-    <div style={style}>
+    <StyledListCard>
       <img alt="nice pants" />
       <h2>{name}</h2>
       <h3>{price}</h3>
       <p>{description}</p>
-    </div>
+    </StyledListCard>
   );
 };
 
