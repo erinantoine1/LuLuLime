d8e9fdc4b53578d7d9a60db7c44cdd1a5092c2e6
Erin Antoine <erin.antoine1@gmail.com>
2022-12-06 14:58:59 -0800

    mostly working add to cart

--- client/src/components/Overview/Overview.jsx
+++ client/src/components/Overview/Overview.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable indent */
 /* eslint-disable no-plusplus */
 /* eslint-disable max-len */
 /* eslint-disable object-curly-newline */
@@ -46,115 +47,197 @@ top: -475px;
 `;
 const AddToCartDiv = styled.div`
 height: 300px;
-width: 400px;
+width: 600px;
 position: relative;
 left: 1100px;
 top: -1175px;
 `;
 
 const Overview = () => {
+  /* ****************** initial state declarations ******************* */
   const [allProducts, setAllProducts] = useState([]);
-  const [allProductStyles, setAllProductStyles] = useState([]);
-  const [currentProductStyle, setCurrentProductStyle] = useState();
-  const [currentOriginalPrice, setCurrentOriginalPrice] = useState('');
-  const [currentSalePrice, setCurrentSalePrice] = useState('');
-  const [currentSizes, setCurrentSizes] = useState([]);
-  const [currentQuantities, setCurrentQuantities] = useState([]);
-  const [currentSize, setCurrentSize] = useState('');
-  const [currentQuantity, setCurrentQuantity] = useState('');
+  const [productId, setProductId] = useState();
+  const [productTitle, setProductTitle] = useState();
+  const [productCategory, setProductCategory] = useState();
+  const [productOverview, setProductOverview] = useState();
 
+  const [allProductStyles, setAllProductStyles] = useState([]);
+  const [productStyleDefault, setProductStyleDefault] = useState();
+  const [productStyleId, setProductStyleId] = useState();
+  const [productStyleName, setProductStyleName] = useState();
+  const [productStyleOriginalPrice, setProductStyleOriginalPrice] = useState();
+  const [productStyleSalePrice, setProductStyleSalePrice] = useState();
+  const [productStylePhotos, setProductStylePhotos] = useState();
+  const [productStyleSku, setProductStyleSku] = useState();
+  const [productStyleSkus, setProductStyleSkus] = useState();
+  const [productStyleSize, setProductStyleSize] = useState();
+  const [productStyleQuantity, setProductStyleQuantity] = useState();
+  const [productStyleSizes, setProductStyleSizes] = useState([]);
+  const [productStyleQuantities, setProductStyleQuantities] = useState([]);
+  const [undefinedSizeSubmitted, setUndefinedSizeSubmitted] = useState(false);
+  const [quantitySelectorIsDisabled, setQuantitySelectorIsDisabled] = useState(true);
+  const [dropdownQuantitiesArray, setDropdownQuantitiesArray] = useState(['-']);
 
   /* ****************** axios requests ******************* */
   useEffect(() => {
     axios.get('/products')
       .then((response) => {
         setAllProducts(response.data);
+        setProductId(response.data[2].id);
+        setProductTitle(response.data[2].name);
+        setProductCategory(response.data[2].category);
+        setProductOverview(response.data[2].description);
       }).then(() => {
         axios.get('/products/40346/styles')
           .then((response) => {
             setAllProductStyles(response.data.results);
-            let defaultIndex;
             for (let i = 0; i < response.data.results.length; i++) {
               if (response.data.results[i]['default?']) {
-                setCurrentProductStyle(response.data.results[i].style_id);
-                defaultIndex = i;
+                setProductStyleDefault(response.data.results[i]['default?']);
+                setProductStyleId(response.data.results[i].style_id);
+                setProductStyleName(response.data.results[i].name);
+                setProductStyleOriginalPrice(response.data.results[i].original_price);
+                setProductStyleSalePrice(response.data.results[i].sale_price);
+                setProductStylePhotos(response.data.results[i].photos);
+                setProductStyleSkus(response.data.results[i].skus);
+
+                const skuKeys = Object.keys(response.data.results[i].skus);
+                const productStyleSizesArray = [];
+                const productStyleQuantitiesArray = [];
+                for (let j = 0; j < skuKeys.length; j++) {
+                  productStyleSizesArray.push(response.data.results[i].skus[skuKeys[j]].size);
+                  productStyleQuantitiesArray.push(response.data.results[i].skus[skuKeys[j]].quantity);
+                }
+
+                setProductStyleSizes(productStyleSizesArray);
+                setProductStyleQuantities(productStyleQuantitiesArray);
               }
-              setCurrentSalePrice(response.data.results[i].sale_price);
-              setCurrentOriginalPrice(response.data.results[i].original_price);
-
-              // const skus = Object.keys(response.data.results[i].skus);
-
-              // // const tempSizeArray = [];
-              // // const tempQuantityArray = [];
-              // const tempSizesArray = ['Select Size'];
-              // const tempQuantitiesArray = [];
-              // for (let j = 0; j < skus.length; j++) {
-              //   // tempSizeArray.push(response.data.results[defaultIndex].skus[skus[j]].size);
-              //   tempSizesArray.push(response.data.results[i].skus[skus[j]].size);
-              //   tempQuantitiesArray.push(response.data.results[i].skus[skus[j]].quantity);
-              // }
-              // setCurrentSizes(tempSizesArray);
-              // setCurrentQuantities(tempQuantitiesArray);
-
-
-
             }
           });
       });
   }, []);
+  const AddToCartSubmit = () => {
 
-  // .then(() => {
-  //   axios.get('/reviews')
-  //     .then((response) => {
-  //       console.log(response.data);
-  //       setAllReviews(response.data);
-  //     });
-  // });
+    const addToCartObject = {
+      sku_id: productStyleSku,
+      count: productStyleQuantity,
+    };
 
-  /* ****************** mapping products ******************* */
-  const prods = (
-    allProducts.map((product) => {
-      return (
-        <div className="allProductsMap">
-          <NavigationDiv>Future Navigation Div</NavigationDiv>
-          <PlaceHolderDiv />
-          <ImageGalleryDiv>
-            <ImageGallery />
-          </ImageGalleryDiv>
-          <br />
-          <StyleSelectorDiv>
-            Select Style:
-            <StyleSelector
-              allProductStyles={allProductStyles}
-              currentProductStyle={currentProductStyle}
-              setCurrentProductStyle={setCurrentProductStyle}
-              setCurrentOriginalPrice={setCurrentOriginalPrice}
-              setCurrentSalePrice={setCurrentSalePrice}
-              setCurrentSizes={setCurrentSizes}
-              setCurrentQuantities={setCurrentQuantities}
-              currentSalePrice={currentSalePrice}
-            />
-          </StyleSelectorDiv>
-          <ProductInformation
-            productTitle={product.name}
-            productCategory={product.category}
-            productDescription={product.description}
-            currentOriginalPrice={currentOriginalPrice}
-            currentSalePrice={currentSalePrice}
+    if (addToCartObject.count === undefined) {
+      addToCartObject.count = 1;
+    }
+
+    if (productStyleSize === undefined || productStyleSize === 'Select Size') {
+      setUndefinedSizeSubmitted(true);
+    }
+
+    for (let i = 0; i < Number(addToCartObject.count); i++) {
+      axios.post('/cart', addToCartObject, { headers: { 'content-type': 'application/json' } }).then(() => {
+        return axios.get('/cart');
+      }).then((response) => {
+        console.log('Database after POST: ', response.data);
+      });
+    }
+  };
+
+    // then set default settings again
+    // setUndefinedSizeSubmitted(false);
+
+    /* ****************** mapping products ******************* */
+    return (
+      <div>
+        <NavigationDiv>Future Navigation Div</NavigationDiv>
+        <PlaceHolderDiv />
+        <ImageGalleryDiv>
+          <ImageGallery />
+        </ImageGalleryDiv>
+        <br />
+        <StyleSelectorDiv>
+          Select Style:
+          <StyleSelector
+            allProductStyles={allProductStyles}
+            setAllProductStyles={setAllProductStyles}
+            productStyleDefault={productStyleDefault}
+            setProductStyleDefault={setProductStyleDefault}
+            productStyleId={productStyleId}
+            setProductStyleId={setProductStyleId}
+            productStyleName={productStyleName}
+            setProductStyleName={setProductStyleName}
+            productStyleOriginalPrice={productStyleOriginalPrice}
+            setProductStyleOriginalPrice={setProductStyleOriginalPrice}
+            productStyleSalePrice={productStyleSalePrice}
+            setProductStyleSalePrice={setProductStyleSalePrice}
+            productStylePhotos={productStylePhotos}
+            setProductStylePhotos={setProductStylePhotos}
+            productStyleSku={productStyleSku}
+            setProductStyleSku={setProductStyleSku}
+            productStyleSkus={productStyleSkus}
+            setProductStyleSkus={setProductStyleSkus}
+            productStyleSize={productStyleSize}
+            setProductStyleSize={setProductStyleSize}
+            productStyleQuantity={productStyleQuantity}
+            setProductStyleQuantity={setProductStyleQuantity}
+            productStyleSizes={productStyleSizes}
+            setProductStyleSizes={setProductStyleSizes}
+            productStyleQuantities={productStyleQuantities}
+            setProductStyleQuantities={setProductStyleQuantities}
+            undefinedSizeSubmitted={undefinedSizeSubmitted}
+            setUndefinedSizeSubmitted={setUndefinedSizeSubmitted}
+            quantitySelectorIsDisabled={quantitySelectorIsDisabled}
+            setQuantitySelectorIsDisabled={setQuantitySelectorIsDisabled}
+            dropdownQuantitiesArray={dropdownQuantitiesArray}
+            setDropdownQuantitiesArray={setDropdownQuantitiesArray}
           />
-          <AddToCartDiv>
-            <AddToCart currentSizes={currentSizes} currentQuantities={currentQuantities} currentSize={currentSize} currentQuantity={currentQuantity} setCurrentSize={setCurrentSize} setCurrentQuantity={setCurrentQuantity} />
-          </AddToCartDiv>
-        </div>
+        </StyleSelectorDiv>
+        <ProductInformation
+          productId={productId}
+          setProductId={setProductId}
+          productTitle={productTitle}
+          setProductTitle={setProductTitle}
+          productCategory={productCategory}
+          setProductCategory={setProductCategory}
+          productOverview={productOverview}
+          setProductOverview={setProductOverview}
+          productStyleOriginalPrice={productStyleOriginalPrice}
+          setProductStyleOriginalPrice={setProductStyleOriginalPrice}
+          productStyleSalePrice={productStyleSalePrice}
+          setProductStyleSalePrice={setProductStyleSalePrice}
+        />
+        <AddToCartDiv>
+          <AddToCart
+            productStyleId={productStyleId}
+            setProductStyleId={setProductStyleId}
+            productStyleName={productStyleName}
+            setProductStyleName={setProductStyleName}
+            productStyleOriginalPrice={productStyleOriginalPrice}
+            setProductStyleOriginalPrice={setProductStyleOriginalPrice}
+            productStyleSalePrice={productStyleSalePrice}
+            setProductStyleSalePrice={setProductStyleSalePrice}
+            productStylePhotos={productStylePhotos}
+            setProductStylePhotos={setProductStylePhotos}
+            productStyleSku={productStyleSku}
+            setProductStyleSku={setProductStyleSku}
+            productStyleSkus={productStyleSkus}
+            setProductStyleSkus={setProductStyleSkus}
+            productStyleSize={productStyleSize}
+            setProductStyleSize={setProductStyleSize}
+            productStyleQuantity={productStyleQuantity}
+            setProductStyleQuantity={setProductStyleQuantity}
+            productStyleSizes={productStyleSizes}
+            setProductStyleSizes={setProductStyleSizes}
+            productStyleQuantities={productStyleQuantities}
+            setProductStyleQuantities={setProductStyleQuantities}
+            AddToCartSubmit={AddToCartSubmit}
+            undefinedSizeSubmitted={undefinedSizeSubmitted}
+            setUndefinedSizeSubmitted={setUndefinedSizeSubmitted}
+            quantitySelectorIsDisabled={quantitySelectorIsDisabled}
+            setQuantitySelectorIsDisabled={setQuantitySelectorIsDisabled}
+            dropdownQuantitiesArray={dropdownQuantitiesArray}
+            setDropdownQuantitiesArray={setDropdownQuantitiesArray}
+          />
+        </AddToCartDiv>
+      </div>
       );
-    })
-  );
-  return prods[2];
-};
-
-/* ****************** check prop-types ******************* */
-Overview.propTypes = {
-  product: PropTypes.object,
 };
 
 export default Overview;
