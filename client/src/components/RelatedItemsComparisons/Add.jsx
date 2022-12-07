72505da1129b77fc27d4c4b8388ea84340ff28ec
Justin <just.inchong4life@gmail.com>
2022-12-06 18:15:40 -0800

    remove unused code/styles, fixing spacing, deleted comments

--- client/src/components/RelatedItemsComparisons/Add.jsx
+++ client/src/components/RelatedItemsComparisons/Add.jsx
@@ -33,5 +33,4 @@ const Add = ({ outfitItems, setOutfitItems }) => {
   );
 };
 
-
 export default Add;
--- client/src/components/RelatedItemsComparisons/RelatedCard.jsx
+++ client/src/components/RelatedItemsComparisons/RelatedCard.jsx
@@ -13,10 +13,6 @@ const StyledCard = styled.div`
   position: relative;
 `;
 
-// &:first-child {
-//   margin-left: 0px;
-// }
-
 const StyledCard2 = styled.div`
   box-shadow: 0 1px 1rem -4px #000;
   background: #fff;
@@ -24,11 +20,6 @@ const StyledCard2 = styled.div`
   overflow: hidden;
   cursor: pointer;
 `;
-
-// &:first-child {
-//   margin-left: 0px;
-// }
-// object-fit: cover;
 
 const Image = styled.img`
   height: 70%;
@@ -65,19 +56,6 @@ const RelatedCard = ({ picture, type, setShowModal, showModal, outfitItems, setO
   const [name, setName] = useState('');
   const [price, setPrice] = useState('');
   // const [picture, setPicture] = useState('');
-
-  const styledcard = useRef(null);
-
-  // useEffect(() => {
-  //   axios.get('https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/products/40346/styles', { headers: { Authorization: TOKEN } })
-  //     .then((res) => {
-  //       console.log(res.data);
-  //       setName(res.data.results[0].name);
-  //       setPrice(res.data.results[0].original_price);
-  //       setPicture(res.data.results[0].photos[0].thumbnail_url);
-  //     })
-  //     .catch((err) => console.error(err));
-  // }, []);
 
   return (
     <StyledCard>
