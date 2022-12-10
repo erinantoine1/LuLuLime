84e89e6c4f4fab0dd70abf226f279e3862f9efa1
Chris Garcia <garciac9203@gmail.com>
2022-12-10 09:02:34 -0800

    Added userevent library for testing

--- client/src/components/RatingsReviews/__RRTests__/RatingsReviews.test.js
+++ client/src/components/RatingsReviews/__RRTests__/RatingsReviews.test.js
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import axios from 'axios';
 import { render, screen, cleanup, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { reviewsTestData, metadataTestData } from './TestData.js';
 
 import RatingsReviews from '../RatingsReviews.jsx';
